8cf7aed93c228444f41e7c9ef7ddedd888d309b3
Ned Ememanka <officialchinedu@gmail.com>
2021-08-01 14:12:04 -0400

    first commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Ned Ememanka on 2021-07-01.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
